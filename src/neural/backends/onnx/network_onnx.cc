d5efbf3aa896ca24dc609d61e5d6bd22319b6302
Menkib <menkib64@proton.me>
2026-08-02 00:14:24 +0300

    Allow concurrent engine build on a multi-GPU system

--- src/neural/backends/onnx/network_onnx.cc
+++ src/neural/backends/onnx/network_onnx.cc
@@ -899,7 +899,7 @@ Ort::SessionOptions OnnxNetwork::GetOptions(int threads, int batch_size,
       trt_options["trt_timing_cache_enable"] = "1";
       trt_options["trt_timing_cache_path"] = cache_dir;
       trt_options["trt_layer_norm_fp32_fallback"] = "1";
-      trt_options["trt_force_sequential_engine_build"] = "1";
+      trt_options["trt_force_sequential_engine_build"] = "0";
       trt_options["trt_context_memory_sharing_enable"] = "1";
       // Looks like we need I/O binding to enable this.
 #ifdef USE_ONNX_CUDART
