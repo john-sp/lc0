1d7341b55804d26e9a8d9e428a1068a69fc02a68
Menkib <menkib64@proton.me>
2026-08-08 14:57:20 +0300

    Move solid tree threshold to shared parameters

--- src/search/classic/params.cc
+++ src/search/classic/params.cc
@@ -535,7 +535,7 @@ const OptionId SearchParams::kMaxPrefetchBatchId{
     "When the engine cannot gather a large enough batch for immediate use, try "
     "to prefetch up to X positions which are likely to be useful soon, and put "
     "them into cache."};
-const OptionId SearchParams::kSolidTreeThresholdId{
+const OptionId BaseSearchParams::kSolidTreeThresholdId{
     "solid-tree-threshold", "SolidTreeThreshold",
     "Only nodes with at least this number of visits will be considered for "
     "solidification for improved cache locality."};
@@ -631,12 +631,12 @@ void BaseSearchParams::Populate(OptionsParser* options) {
   options->Add<FloatOption>(kUCIRatingAdvId, -10000.0f, 10000.0f) = 0.0f;
   options->Add<BoolOption>(kSearchSpinBackoffId) = false;
   options->Add<FloatOption>(kGarbageCollectionDelayId, 0.0f, 100.0f) = 10.0f;
+  options->Add<IntOption>(kSolidTreeThresholdId, 1, 2000000000) = 100;
 }
 
 void SearchParams::Populate(OptionsParser* options) {
   BaseSearchParams::Populate(options);
   options->Add<IntOption>(kMaxPrefetchBatchId, 0, 1024) = DEFAULT_MAX_PREFETCH;
-  options->Add<IntOption>(kSolidTreeThresholdId, 1, 2000000000) = 100;
 }
 
 BaseSearchParams::BaseSearchParams(const OptionsDict& options)
@@ -725,10 +725,10 @@ BaseSearchParams::BaseSearchParams(const OptionsDict& options)
       kMaxCollisionVisitsScalingPower(
           options.Get<float>(kMaxCollisionVisitsScalingPowerId)),
       kSearchSpinBackoff(options_.Get<bool>(kSearchSpinBackoffId)),
+      kSolidTreeThreshold(options.Get<int>(kSolidTreeThresholdId)),
       kGarbageCollectionDelay(options_.Get<float>(kGarbageCollectionDelayId)) {}
 
 SearchParams::SearchParams(const OptionsDict& options)
-    : BaseSearchParams(options),
-      kSolidTreeThreshold(options.Get<int>(kSolidTreeThresholdId)) {}
+    : BaseSearchParams(options) {}
 }  // namespace classic
 }  // namespace lczero
